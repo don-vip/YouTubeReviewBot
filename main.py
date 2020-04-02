cabac225f9162ae193b9abaa2a20e4f9812e6793
I'm User:Eatcha <51746381+eatcha-wikimedia@users.noreply.github.com>
2020-04-02 10:01:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -271,11 +271,11 @@ def checkfiles():
                         continue
                     else:pass
                     not_available_new_text = re.sub("</gallery>", "%s|Uploader Name : %s <br> video url : %s <br> oldest archive : %s \n</gallery>" % ( filename, uploader(filename,link=True), OriginalURL , oldest_archive_url) , not_available_old_text)
-                    EditSummary = "Adding [[%s]] which is uploaded by %s" % (filename, uploader(filename,link=True))
+                    EditSummary = "Adding [[%s]], was uploaded by %s" % (filename, uploader(filename,link=True))
                     try:
                         commit(not_available_old_text, not_available_new_text, not_available_page, EditSummary)
                     except pywikibot.LockedPage as error:
-                        print(colored("Page is locked '%s'." % error, 'red'))
+                        out("Page is locked '%s'." % error, 'red'))
                         continue
             else:
                 pass
